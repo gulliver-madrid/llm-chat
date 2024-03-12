d02fb7f2f9dd35e01226afa43040b9d5d30fc5d8
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-12 16:22:22 +0100

    Extract select_model() to model_choice.py

--- src/main.py
+++ src/main.py
@@ -5,16 +5,16 @@ from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage, ChatCompletionResponse
 from rich import print
 
+from src.ahora import get_current_time
 from src.io_helpers import (
-    CALL_TO_ACTION,
     HIGHLIGHT_ROLE,
     NEUTRAL_MSG,
     end,
     get_input,
     show_error_msg,
 )
+from src.model_choice import select_model
 
-from src.ahora import get_current_time
 
 modelos: Final[Sequence[str]] = ["tiny", "small", "medium", "large-2402"]
 
@@ -57,61 +57,10 @@ class MenuManager:
         return salir
 
 
-def parse_model_choice(modelos: Sequence[str], eleccion: str) -> str | None:
-    # Asegurarse de que la eleccion es valida
-    try:
-        eleccion_numerica = int(eleccion)
-    except ValueError:
-        show_error_msg(
-            "Entrada no válida. Por favor introduce un número válido o solo pulsa Enter para seleccionar el modelo por defecto."
-        )
-        return None
-
-    if 1 <= eleccion_numerica <= len(modelos):
-        modelo_elegido = modelos[eleccion_numerica - 1]
-        return modelo_elegido
-    else:
-        show_error_msg(
-            "Número fuera de rango. Por favor introduce un número válido o solo pulsa Enter para seleccionar el modelo por defecto."
-        )
-        return None
-
-
-def elegir_modelo() -> str:
-    """Prompt the user to choose a model. Returns the model name without the 'mistral' preffix."""
-    default_model = modelos[0]
-    while True:
-        # Mostrar opciones al usuario
-        print(
-            CALL_TO_ACTION
-            + "\nPor favor, elige un modelo introduciendo el número correspondiente:"
-        )
-        for i, modelo in enumerate(modelos, start=1):
-            print(f"{i}. mixtral-{modelo}")
-        print(
-            NEUTRAL_MSG
-            + f"\nPresiona enter sin seleccionar un número para elegir el modelo [blue_violet]mistral-{default_model}[/blue_violet] por defecto."
-        )
-
-        # Leer la eleccion del usuario
-        num_opciones = len(modelos)
-        eleccion = get_input(f"[bold]Introduce tu elección (1-{num_opciones})")
-
-        if not eleccion:
-            modelo_elegido = default_model
-        else:
-            modelo_elegido = parse_model_choice(modelos, eleccion)
-        if modelo_elegido:
-            break
-
-    print(f"\nModelo elegido: mistral-{modelo_elegido}")
-    return modelo_elegido
-
-
 def main() -> None:
     """Runs the text interface to Mistral models"""
     api_key = os.environ["MISTRAL_API_KEY"]
-    model = "mistral-" + elegir_modelo()
+    model = "mistral-" + select_model(modelos)
     chat_response = None
 
     client = MistralClient(api_key=api_key)
