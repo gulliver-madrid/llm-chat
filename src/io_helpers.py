9263139cc6c0cf043bbda3ec1f1b0375448ee9e6
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-12 11:43:45 +0100

    extract constants and functions to io_helpers.py

---
+++ src/io_helpers.py
@@ -0,0 +1,30 @@
+import time
+
+from rich import print
+
+ERROR = "[bright_red]"
+CALL_TO_ACTION = "[bright_cyan]"
+HIGHLIGHT_ROLE = "[light_green]"
+NEUTRAL_MSG = "[dark_goldenrod]"
+
+
+def end(s: str) -> str:
+    """Create a end tag"""
+    assert s[0] == "["
+    assert s[-1] == "]"
+    return f"[/{s[1:-1]}]"
+
+
+def get_input(text: str) -> str:
+    """Prompts the user to provide an input, in a styled way"""
+    print(
+        CALL_TO_ACTION + f"\n{text}\n> ",
+        end="",
+    )
+    return input()
+
+
+def show_error_msg(text: str) -> None:
+    """Displays an error message with the given text and make a short pause before returning"""
+    print(ERROR + f"\n{text}")
+    time.sleep(1)
