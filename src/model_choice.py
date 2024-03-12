471e53ec2d8d0669efe9f8e69168b23bed688159
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-12 16:56:34 +0100

    extract apply_tag()

--- src/model_choice.py
+++ src/model_choice.py
@@ -2,7 +2,15 @@ from typing import Sequence
 
 from rich import print
 
-from src.io_helpers import CALL_TO_ACTION, NEUTRAL_MSG, get_input, show_error_msg
+from src.io_helpers import (
+    BLUE_VIOLET_COLOR,
+    BOLD_STYLE,
+    CALL_TO_ACTION,
+    NEUTRAL_MSG,
+    apply_tag,
+    get_input,
+    show_error_msg,
+)
 
 MODEL_PREFIX = "mistral"
 
@@ -33,19 +41,32 @@ def select_model(modelos: Sequence[str]) -> str:
     while True:
         # Mostrar opciones al usuario
         print(
-            CALL_TO_ACTION
-            + "\nPor favor, elige un modelo introduciendo el número correspondiente:"
+            apply_tag(
+                "\nPor favor, elige un modelo introduciendo el número correspondiente:",
+                CALL_TO_ACTION,
+            )
         )
         for i, modelo in enumerate(modelos, start=1):
             print(f"{i}. {MODEL_PREFIX}-{modelo}")
-        print(
-            NEUTRAL_MSG
-            + f"\nPresiona enter sin seleccionar un número para elegir el modelo [blue_violet]{MODEL_PREFIX}-{default_model}[/blue_violet] por defecto."
+        model_name_styled = apply_tag(
+            f"{MODEL_PREFIX}-{default_model}", BLUE_VIOLET_COLOR
         )
+        default_model_explanation = (
+            "\nPresiona enter sin seleccionar un número para elegir el modelo "
+            + model_name_styled
+            + " por defecto."
+        )
+        styled_default_model_explanation = apply_tag(
+            default_model_explanation,
+            NEUTRAL_MSG,
+        )
+        print(styled_default_model_explanation)
 
         # Leer la eleccion del usuario
         num_opciones = len(modelos)
-        eleccion = get_input(f"[bold]Introduce tu elección (1-{num_opciones})")
+        eleccion = get_input(
+            apply_tag(f"Introduce tu elección (1-{num_opciones})", BOLD_STYLE)
+        )
 
         if not eleccion:
             modelo_elegido = default_model
