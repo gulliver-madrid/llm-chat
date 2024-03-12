75a2268d6514303cbfc567f2a6ebcacf14e245b3
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-12 11:43:45 +0100

    extract constants and functions to io_helpers.py

--- src/main.py
+++ src/main.py
@@ -1,41 +1,22 @@
 import os
-import time
 from typing import Final, Sequence
 
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage, ChatCompletionResponse
 from rich import print
 
-from .ahora import get_current_time
+from src.io_helpers import (
+    CALL_TO_ACTION,
+    HIGHLIGHT_ROLE,
+    NEUTRAL_MSG,
+    end,
+    get_input,
+    show_error_msg,
+)
+
+from src.ahora import get_current_time
 
 modelos: Final[Sequence[str]] = ["tiny", "small", "medium", "large-2402"]
-
-ERROR = "[bright_red]"
-CALL_TO_ACTION = "[bright_cyan]"
-HIGHLIGHT_ROLE = "[light_green]"
-NEUTRAL_MSG = "[dark_goldenrod]"
-
-
-def end(s: str) -> str:
-    """Create a end tag"""
-    assert s[0] == "["
-    assert s[-1] == "]"
-    return f"[/{s[1:-1]}]"
-
-
-def get_input(text: str) -> str:
-    """Prompts the user to provide an input, in a styled way"""
-    print(
-        CALL_TO_ACTION + f"\n{text}\n> ",
-        end="",
-    )
-    return input()
-
-
-def show_error_msg(text: str) -> None:
-    """Displays an error message with the given text and make a short pause before returning"""
-    print(ERROR + f"\n{text}")
-    time.sleep(1)
 
 
 def print_interaction(model: str, question: str, content: str) -> None:
@@ -48,7 +29,7 @@ def print_interaction(model: str, question: str, content: str) -> None:
 class MenuManager:
     @staticmethod
     def enter_debug_mode(response: ChatCompletionResponse | None) -> None:
-        from .debug import show  # pyright: ignore [reportUnusedImport]
+        from src.debug import show  # pyright: ignore [reportUnusedImport]
 
         print(NEUTRAL_MSG + "Entrando en modo de depuracion\n")
         print(response)
