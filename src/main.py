e992fd865f383a1a57502f10b2b88777f69d5372
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-12 17:10:27 +0100

    create Main(), refactor MenuManager to return an optional Action

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ from src.io_helpers import (
     get_input,
     highlight_role,
 )
-from src.menu_manager import MenuManager
+from src.menu_manager import SALIR, MenuManager
 from src.model_choice import MODEL_PREFIX, select_model
 
 
@@ -25,44 +25,51 @@ def print_interaction(model: str, question: str, content: str) -> None:
     print("\n" + highlight_role(model.upper() + ": ") + content)
 
 
+class Main:
+
+    def execute(self) -> None:
+        """Runs the text interface to Mistral models"""
+        api_key = os.environ["MISTRAL_API_KEY"]
+        model = MODEL_PREFIX + "-" + select_model(modelos)
+        chat_response = None
+
+        client = MistralClient(api_key=api_key)
+
+        while True:
+            # modo multilinea por defecto
+            question = get_input(
+                "Introduce tu consulta (o pulsa Enter para ver más opciones). Introduce `end` como único contenido de una línea cuando hayas terminado."
+            )
+
+            if not question:
+                action = MenuManager.enter_inner_menu(chat_response)
+                if not action:
+                    continue
+                if action.name == SALIR:
+                    break
+                raise RuntimeError(f"Acción no válida: {action}")
+
+            assert question
+
+            while (more := input()).lower() != "end":
+                question += "\n" + more
+
+            print("...procesando")
+
+            chat_response = client.chat(
+                model=model,
+                messages=[ChatMessage(role="user", content=question)],
+            )
+            choices = chat_response.choices
+            content = choices[0].message.content
+            assert isinstance(content, str)
+            print_interaction(model, question, content)
+
+
 def main() -> None:
-    """Runs the text interface to Mistral models"""
-    api_key = os.environ["MISTRAL_API_KEY"]
-    model = MODEL_PREFIX + "-" + select_model(modelos)
-    chat_response = None
-
-    client = MistralClient(api_key=api_key)
-
-    while True:
-        # modo multilinea por defecto
-        question = get_input(
-            "Introduce tu consulta (o pulsa Enter para ver más opciones). Introduce `end` como único contenido de una línea cuando hayas terminado."
-        )
-
-        if not question:
-            salir = MenuManager.enter_inner_menu(chat_response)
-            if salir:
-                break
-            continue
-
-        assert question
-
-        while (more := input()).lower() != "end":
-            question += "\n" + more
-
-        print("...procesando")
-
-        chat_response = client.chat(
-            model=model,
-            messages=[ChatMessage(role="user", content=question)],
-        )
-        choices = chat_response.choices
-        content = choices[0].message.content
-        assert isinstance(content, str)
-        print_interaction(model, question, content)
-
+    main_instance = Main()
+    main_instance.execute()
     print(NEUTRAL_MSG + "Saliendo")
-    exit()
 
 
 if __name__ == "__main__":
