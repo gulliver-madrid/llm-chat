676ac01cd7160128ead7725008e2840ad6329da1
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-13 11:16:17 +0100

    support for "/for" in placeholders

--- src/main.py
+++ src/main.py
@@ -61,15 +61,44 @@ class Main:
                 for placeholder in set_occurrences:
                     subs = get_input("Por favor indica el valor de " + placeholder)
                     substitutions[placeholder] = subs
-                for placeholder, subs in substitutions.items():
-                    question = question.replace(placeholder, subs)
-
+                for_placeholders = [
+                    placeholder
+                    for placeholder, subs in substitutions.items()
+                    if subs.startswith("/for")
+                ]
+                if len(for_placeholders) > 1:
+                    print(
+                        "El uso de varios '/for' con los placeholders no está soportado"
+                    )
+                    continue
+                elif len(for_placeholders) == 1:
+                    placeholder_with_for = for_placeholders[0]
+                    subs_in_for_str = substitutions[placeholder_with_for]
+                    subs_in_for_str = subs_in_for_str.removeprefix("/for")
+                    subs_in_for = subs_in_for_str.split(",")
+                    questions: list[str] = []
+                    for sub in subs_in_for:
+                        questions.append(question.replace(placeholder_with_for, sub))
+                    del substitutions[placeholder_with_for]
+                    new_questions: list[str] = []
+                    for question in questions:
+                        for placeholder, subs in substitutions.items():
+                            question = question.replace(placeholder, subs)
+                        new_questions.append(question)
+                    questions = new_questions
+                else:
+                    for placeholder, subs in substitutions.items():
+                        question = question.replace(placeholder, subs)
+                    questions = [question]
                 print("Placeholders sustituidos exitosamente")
+            else:
+                questions = [question]
 
-            print("\n...procesando")
+            for i, question in enumerate(questions):
+                print("\n...procesando consulta número", i + 1)
 
-            content = client_wrapper.get_simple_response(model, question)
-            print_interaction(model, question, content)
+                content = client_wrapper.get_simple_response(model, question)
+                print_interaction(model, question, content)
 
 
 def main() -> None:
