59194aaae4f7d0007effa7073214bea923e56a76
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-14 12:49:00 +0100

    refactor test_build_questions.py

--- tests/test_build_questions.py
+++ tests/test_build_questions.py
@@ -4,37 +4,50 @@ from src.main import build_questions
 from src.placeholders import Placeholder
 
 
-def as_substitution_dict(d: dict[str, str]) -> dict[Placeholder, str]:
+def as_substitutions(d: dict[str, str]) -> dict[Placeholder, str]:
     assert isinstance(d, dict)
     assert all(isinstance(k, str) and isinstance(v, str) for k, v in d.items())
     return cast(dict[Placeholder, str], d)
+
+
+START_TO_END_RAW_QUESTION = "List numbers from {start} to {end}"
+WHAT_IS_THE_CAPITAL_OF_FRANCE_QUESTION = "What is the capital of France?"
 
 
 class TestBuildQuestions(unittest.TestCase):
     """The format of the placeholders is irrelevant here"""
 
     def test_build_questions_no_placeholders(self) -> None:
-        raw_question = "What is the capital of France?"
-        substitutions = as_substitution_dict({})
-        expected = [raw_question]
-        self.assertEqual(build_questions(raw_question, substitutions), expected)
+        substitutions = as_substitutions({})
+        expected = [WHAT_IS_THE_CAPITAL_OF_FRANCE_QUESTION]
+        self.assertEqual(
+            build_questions(WHAT_IS_THE_CAPITAL_OF_FRANCE_QUESTION, substitutions),
+            expected,
+        )
 
     def test_build_questions_with_placeholders(self) -> None:
         raw_question = "What is the capital of {country}?"
-        substitutions = as_substitution_dict({"{country}": "France"})
-        expected = ["What is the capital of France?"]
+        substitutions = as_substitutions({"{country}": "France"})
+        expected = [WHAT_IS_THE_CAPITAL_OF_FRANCE_QUESTION]
         self.assertEqual(build_questions(raw_question, substitutions), expected)
 
-    def test_build_questions_with_for_command(self) -> None:
-        raw_question = "List numbers from {start} to {end}"
-        substitutions = as_substitution_dict({"{start}": "/for 1", "{end}": "5"})
+    def test_build_questions_with_for_command_with_one_element(self) -> None:
+        substitutions = as_substitutions({"{start}": "/for 1", "{end}": "5"})
         expected = ["List numbers from 1 to 5"]
-        self.assertEqual(build_questions(raw_question, substitutions), expected)
+        self.assertEqual(
+            build_questions(START_TO_END_RAW_QUESTION, substitutions), expected
+        )
+
+    def test_build_questions_with_for_command_with_two_elements(self) -> None:
+        substitutions = as_substitutions({"{start}": "/for 10,20", "{end}": "50"})
+        expected = ["List numbers from 10 to 50", "List numbers from 20 to 50"]
+        self.assertEqual(
+            build_questions(START_TO_END_RAW_QUESTION, substitutions), expected
+        )
 
     def test_build_questions_multiple_for_not_supported(self) -> None:
-        raw_question = "List numbers from {start} to {end}"
-        substitutions = as_substitution_dict({"{start}": "/for 1", "{end}": "/for 5"})
-        self.assertIsNone(build_questions(raw_question, substitutions))
+        substitutions = as_substitutions({"{start}": "/for 1", "{end}": "/for 5"})
+        self.assertIsNone(build_questions(START_TO_END_RAW_QUESTION, substitutions))
 
 
 if __name__ == "__main__":
