7ce3e74c68569a4af5fd19fe57a40189c6052b1b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-15 11:19:59 +0100

    remove debug module

--- src/debug.py
+++
@@ -1,7 +0,0 @@
-from rich import print
-
-
-def show(chat_response: object) -> None:
-    for attr in dir(chat_response):
-        if not attr.startswith("_"):
-            print("\n", attr, getattr(chat_response, attr))
