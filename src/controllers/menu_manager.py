9b80ce7d01c638eb96877c64362a0cf7ab3b4725
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-16 09:15:26 +0100

    move show_help to main.py
    - rename MenuManager > CommandInterpreter.

--- src/controllers/menu_manager.py
+++ src/controllers/menu_manager.py
@@ -1,16 +1,14 @@
 from dataclasses import dataclass
 
-from rich.console import Console
-from rich.markdown import Markdown
 
-from src.io_helpers import get_input, show_error_msg
+from src.io_helpers import show_error_msg
 
 
 class ActionName:
     SALIR = "SALIR"
     CHANGE_MODEL = "CHANGE_MODEL"
-    NEW_QUERY = "NEW_QUERY"
     DEBUG = "DEBUG"
+    HELP = "HELP"
 
 
 @dataclass
@@ -18,10 +16,10 @@ class Action:
     name: str
 
 
-class MenuManager:
+class CommandInterpreter:
 
     @staticmethod
-    def enter_inner_menu(raw_question: str) -> Action | None:
+    def parse_user_input(raw_question: str) -> Action | None:
         possible_commands = raw_question.strip().split()
         if not possible_commands:
             return None
@@ -31,26 +29,11 @@ class MenuManager:
             case "/d" | "/debug":
                 return Action(ActionName.DEBUG)
             case "/h" | "/help":
-                MenuManager.show_help()
-                get_input("Pulsa Enter para continuar")
-                return Action(ActionName.NEW_QUERY)
+                return Action(ActionName.HELP)
             case "/change":
                 return Action(ActionName.CHANGE_MODEL)
             case other if other.startswith("/"):
                 show_error_msg("Comando no válido")
+                return None
             case _:
                 return None
-
-    @staticmethod
-    def show_help():
-        console = Console()
-        markdown = Markdown(
-            """
-## Consultas
-Puedes usar placeholders con el formato `$0<nombre>`. Ejemplo: `¿Quién fue $0persona y que hizo en el ámbito de $0tema?` El programa te pedirá luego que completes los placeholders uno por uno.
-Si empiezas el contenido de un placeholder con `/for` y pones las variantes separadas por comas, se generará una consulta con cada variante. Por ejemplo, si en la pregunta anterior introduces como valor de $0persona `/for Alexander Flemming,Albert Einstein` se generarán 2 consultas, una para cada nombre introducido.
-### Comandos
-Puedes iniciar tu consulta con `/d` para activar el modo depuración.
-"""
-        )
-        console.print(markdown, width=60)
