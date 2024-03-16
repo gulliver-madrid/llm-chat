c81aa487167bb0279f67859423c74071e2b67d96
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-16 14:58:02 +0100

    explain the command /new in both the README file and the internal help

--- src/main.py
+++ src/main.py
@@ -33,6 +33,7 @@ HELP_TEXT = """
 Puedes usar placeholders con el formato `$0<nombre>`. Ejemplo: `¿Quién fue $0persona y que hizo en el ámbito de $0tema?` El programa te pedirá luego que completes los placeholders uno por uno.
 Si empiezas el contenido de un placeholder con `/for` y pones las variantes separadas por comas, se generará una consulta con cada variante. Por ejemplo, si en la pregunta anterior introduces como valor de $0persona `/for Alexander Flemming,Albert Einstein` se generarán 2 consultas, una para cada nombre introducido.
 ### Comandos
+Para empezar una nueva conversación en lugar de seguir con la actual, usa el comando `/new` al inicio de tu consulta.
 Puedes iniciar tu consulta con `/d` para activar el modo depuración.
 """
 
