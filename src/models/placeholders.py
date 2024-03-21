560a8ab467df877238756c963a40de9a38482789
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-21 10:29:17 +0100

    refactor placeholders substitution

--- src/models/placeholders.py
+++ src/models/placeholders.py
@@ -1,9 +1,14 @@
 import re
-from typing import Mapping, NewType
+from typing import Final, Mapping, NewType
 
 from src.utils import remove_duplicates
 
+__all__ = ["build_queries", "find_unique_placeholders"]
+
 Placeholder = NewType("Placeholder", str)
+
+# Data structure mapping placeholders to their replacements
+Substitutions = Mapping[Placeholder, str]
 
 FOR_COMMAND_PREFFIX = "/for"
 
@@ -11,32 +16,33 @@ FOR_COMMAND_PREFFIX = "/for"
 class QueryBuildException(Exception): ...
 
 
-def build_queries(
-    raw_query: str, substitutions: Mapping[Placeholder, str]
-) -> list[str]:
+class MultipleForNotSupported(QueryBuildException):
+    def __init__(self) -> None:
+        super().__init__(
+            f"El uso de varios '{FOR_COMMAND_PREFFIX}' con los placeholders no está soportado"
+        )
+
+
+def build_queries(raw_query: str, substitutions: Substitutions) -> list[str]:
     """
     Constructs a list of queries by replacing placeholders in the raw query with user-provided substitutions.
     If a 'for' command is detected, it generates multiple queries by iterating over the specified range.
 
     Args:
         raw_query: The original query template containing placeholders.
-        substitutions: A dictionary mapping placeholders to their substitutions.
+        substitutions: See alias definition.
 
     Returns:
-        A list of queries with placeholders replaced by their substitutions, or None if an error occurs.
+        A list of queries with all placeholders replaced.
     """
-    placeholders_with_for = get_placeholders_with_for(substitutions)
-    number_of_placeholders_with_for = len(placeholders_with_for)
-    if number_of_placeholders_with_for > 1:
-        raise QueryBuildException(
-            f"El uso de varios '{FOR_COMMAND_PREFFIX}' con los placeholders no está soportado"
-        )
-    elif number_of_placeholders_with_for == 1:
-        queries = replace_placeholders_with_one_for(
-            raw_query, substitutions, placeholders_with_for[0]
-        )
-    else:
-        queries = [replace_query_with_substitutions(raw_query, substitutions)]
+    match _get_placeholders_with_for(substitutions):
+        case []:
+            queries = [_replace_single_placeholders(raw_query, substitutions)]
+        case [placeholder]:
+            replacer = OneForReplacer(substitutions, placeholder)
+            queries = replacer.replace_placeholders_with_one_for(raw_query)
+        case _:
+            raise MultipleForNotSupported()
     return queries
 
 
@@ -55,55 +61,60 @@ def find_unique_placeholders(s: str) -> list[Placeholder]:
     return remove_duplicates(pattern.findall(s))
 
 
-def replace_placeholders_with_one_for(
-    query: str,
-    substitutions: Mapping[Placeholder, str],
-    placeholder_with_for: Placeholder,
-) -> list[str]:
-    replacements_in_for_str = substitutions[placeholder_with_for]
-    assert replacements_in_for_str.startswith(FOR_COMMAND_PREFFIX)
-    replacements_in_for_str = replacements_in_for_str.removeprefix(
-        FOR_COMMAND_PREFFIX
-    ).strip()
-
-    replacements_in_for = replacements_in_for_str.split(",")
-    queries: list[str] = []
-    for replacement in replacements_in_for:
-        queries.append(query.replace(placeholder_with_for, replacement))
-    substitutions = {
-        placeholder: value
-        for placeholder, value in substitutions.items()
-        if placeholder != placeholder_with_for
-    }
-    new_queries: list[str] = []
-    for query in queries:
-        new_queries.append(replace_query_with_substitutions(query, substitutions))
-    queries = new_queries
-    return queries
-
-
-def replace_query_with_substitutions(
-    query: str, substitutions: Mapping[Placeholder, str]
-) -> str:
-    for placeholder, replacement in substitutions.items():
-        query = query.replace(placeholder, replacement)
-    return query
-
-
-def get_placeholders_with_for(
-    substitutions: Mapping[Placeholder, str]
-) -> list[Placeholder]:
+def _get_placeholders_with_for(substitutions: Substitutions) -> list[Placeholder]:
     """
-    Identifies placeholders that are associated with a 'for' command in the substitutions.
-
-    Args:
-        substitutions: A mapping of placeholders to their substitutions.
-
-    Returns:
-        A list of placeholders that have a 'for' command as their substitution.
+    Returns a list of placeholders that are associated with a 'for' command in the substitutions.
     """
     return [
         placeholder
         for placeholder, subs in substitutions.items()
         if subs.startswith(FOR_COMMAND_PREFFIX)
     ]
+
+
+class OneForReplacer:
+    def __init__(self, substitutions: Substitutions, placeholder_with_for: Placeholder):
+        self._substitutions: Final = substitutions
+        self._placeholder_with_for: Final = placeholder_with_for
+
+    def replace_placeholders_with_one_for(self, query: str) -> list[str]:
+        """
+        Replace placeholders when there is only one for-style replacement.
+
+        Args:
+            query (str): The initial query containing placeholders.
+            substitutions (Substitutions): See alias definition.
+            placeholder_with_for (Placeholder): Placeholder to be replaced with 'for' values.
+
+        Returns:
+            list[str]: List of new queries after replacing placeholders.
+        """
+        substitutions_dict = dict(self._substitutions)
+        del substitutions_dict[self._placeholder_with_for]
+
+        # Create a query for each 'for' value
+        queries = [
+            query.replace(self._placeholder_with_for, replacement)
+            for replacement in self._get_for_replacements()
+        ]
+
+        # Replace the remaining placeholders in the queries
+        return [
+            _replace_single_placeholders(query, substitutions_dict) for query in queries
+        ]
+
+    def _get_for_replacements(self) -> list[str]:
+        """Extract the list of individual replacements"""
+        for_values_str = self._substitutions[self._placeholder_with_for]
+        return self._remove_for_command(for_values_str).split(",")
+
+    def _remove_for_command(self, raw_string: str) -> str:
+        if not raw_string.startswith(command := FOR_COMMAND_PREFFIX):
+            raise ValueError(f"{raw_string!r} no empieza por el comando {command}")
+        return raw_string.removeprefix(command).strip()
+
+
+def _replace_single_placeholders(query: str, substitutions: Substitutions) -> str:
+    for placeholder, replacement in substitutions.items():
+        query = query.replace(placeholder, replacement)
+    return query
