7110019f8fa2891c129fdd33df8eb7e55a3b900c
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-21 08:56:31 +0100

    use find_unique_placeholders()

--- tests/test_find_placeholders.py
+++ tests/test_find_placeholders.py
@@ -1,11 +1,13 @@
 import unittest
 
-from src.models.placeholders import find_placeholders
+from src.models.placeholders import find_unique_placeholders
 
 class TestFindPlaceholders(unittest.TestCase):
 
     def test_find_placeholders(self) -> None:
-        self.assertEqual(find_placeholders("there is not plaholders here"),[])
+        self.assertEqual(find_unique_placeholders("there is not plaholders here"),[])
+        self.assertEqual(find_unique_placeholders("there is a plaholders here: $0placeholder"),["$0placeholder"])
+        self.assertEqual(find_unique_placeholders("there is a unique plaholder here: $0placeholder and $0placeholder"),["$0placeholder"])
 
 
 if __name__ == '__main__':
