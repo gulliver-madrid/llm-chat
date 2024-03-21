a94f9073cf45a1ea97e1def6583f8fb4f5281b63
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-21 08:49:55 +0100

    add TestFindPlaceholders

--- src/models/placeholders.py
+++ src/models/placeholders.py
@@ -48,7 +48,8 @@ def find_placeholders(s: str) -> list[Placeholder]:
     Returns:
         A list of placeholders found in the string.
     """
-    return re.findall(r"(?<![a-zA-Z0-9])\$0[a-zA-Z_][a-zA-Z]*[0-9]*", s)
+    pattern= re.compile(r"(?<![a-zA-Z0-9])\$0[a-zA-Z_][a-zA-Z]*[0-9]*")
+    return pattern.findall(s)
 
 
 def replace_placeholders_with_one_for(
