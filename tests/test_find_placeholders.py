1e488b7e1fa13f293ee46d908c0fc0412a3adfcd
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-21 09:01:02 +0100

    improve placeholder recognition
    - improve regex readability
    - simplify test_find_placeholders

--- tests/test_find_placeholders.py
+++ tests/test_find_placeholders.py
@@ -1,14 +1,49 @@
-import unittest
+from dataclasses import dataclass
 
 from src.models.placeholders import find_unique_placeholders
 
-class TestFindPlaceholders(unittest.TestCase):
 
-    def test_find_placeholders(self) -> None:
-        self.assertEqual(find_unique_placeholders("there is not plaholders here"),[])
-        self.assertEqual(find_unique_placeholders("there is a plaholders here: $0placeholder"),["$0placeholder"])
-        self.assertEqual(find_unique_placeholders("there is a unique plaholder here: $0placeholder and $0placeholder"),["$0placeholder"])
+@dataclass(frozen=True)
+class Case:
+    expected: list[str]
+    text: str
 
 
-if __name__ == '__main__':
-    unittest.main()
+cases = [
+    Case(expected=[], text="there is not placeholders here"),
+    Case(expected=["$0placeholder"], text="there is a placeholder here: $0placeholder"),
+    Case(
+        expected=["$0placeholder"],
+        text="there is a unique placeholder here: $0placeholder and $0placeholder",
+    ),
+    Case(
+        expected=["$0placeholder_1", "$0placeholder_2"],
+        text="there are two placeholders here: $0placeholder_1 and $0placeholder_2",
+    ),
+    Case(
+        expected=["$0placeholder_1"],
+        text="a placeholder followed by a comma: $0placeholder_1, and...",
+    ),
+    Case(
+        expected=[],
+        text="this is not a valid placeholder: $0placeholder10more_text_after_the_digits",
+    ),
+]
+
+
+def test_find_placeholders() -> None:
+    for case in cases:
+        result = find_unique_placeholders(case.text)
+        assert (
+            case.expected == result
+        ), f"""
+
+En la cadena:
+\t{case.text!r}
+
+Deberían haberse encontrado:
+\t{case.expected}
+
+Pero se encontraron:
+\t{result}
+"""
