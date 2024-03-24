7c69db1327cbde2f1ea13b94c41cb78a2166d9d7
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-25 00:15:15 +0100

    convert ActionName into Enum, refactor test

--- tests/test_command_interpreter.py
+++ tests/test_command_interpreter.py
@@ -1,54 +1,39 @@
+from dataclasses import dataclass
 import pytest
 from src.controllers.command_interpreter import (
-    ActionName,
+    ActionType,
     CommandInterpreter,
     CommandNoValid,
 )
 
 
+@dataclass(frozen=True)
+class Case:
+    raw_query: str
+    expected_action_name: ActionType
+    expected_rest_query: str
+
+
 def test_command_interpreter_help() -> None:
     command_interpreter = CommandInterpreter()
-    action, rest_query = command_interpreter.parse_user_input("/help")
-    assert action
-    assert action.name == ActionName.HELP
-    assert rest_query == ""
+    for case in [
+        Case("/help", ActionType.HELP, ""),
+        Case("/debug hola que tal", ActionType.DEBUG, "hola que tal"),
+        Case("/new hola que tal", ActionType.NEW_CONVERSATION, "hola que tal"),
+        Case("/load 5555", ActionType.LOAD_CONVERSATION, "5555"),
+        Case(
+            "/sys Eres un asistente experto.",
+            ActionType.SYSTEM_PROMPT,
+            "Eres un asistente experto.",
+        ),
+    ]:
+        action, rest_query = command_interpreter.parse_user_input(case.raw_query)
+        assert action
+        assert action.name == case.expected_action_name
+        assert rest_query == case.expected_rest_query
 
 
 def test_command_interpreter_wrong_command() -> None:
     command_interpreter = CommandInterpreter()
     with pytest.raises(CommandNoValid):
         command_interpreter.parse_user_input("/helper")
-
-
-def test_command_interpreter_debug() -> None:
-    command_interpreter = CommandInterpreter()
-    action, rest_query = command_interpreter.parse_user_input("/debug hola que tal")
-    assert action
-    assert action.name == ActionName.DEBUG
-    assert rest_query == "hola que tal"
-
-
-def test_command_interpreter_new() -> None:
-    command_interpreter = CommandInterpreter()
-    action, rest_query = command_interpreter.parse_user_input("/new hola que tal")
-    assert action
-    assert action.name == ActionName.NEW_CONVERSATION
-    assert rest_query == "hola que tal"
-
-
-def test_command_interpreter_load() -> None:
-    command_interpreter = CommandInterpreter()
-    action, rest_query = command_interpreter.parse_user_input("/load 5555")
-    assert action
-    assert action.name == ActionName.LOAD_CONVERSATION
-    assert rest_query == "5555"
-
-
-def test_command_interpreter_system() -> None:
-    command_interpreter = CommandInterpreter()
-    action, rest_query = command_interpreter.parse_user_input(
-        "/sys Eres un asistente experto."
-    )
-    assert action
-    assert action.name == ActionName.SYSTEM_PROMPT
-    assert rest_query == "Eres un asistente experto."
