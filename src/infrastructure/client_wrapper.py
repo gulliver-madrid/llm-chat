28c9708f4e20409bdf7c0b35f2065e09c4e71ac2
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-24 21:01:42 +0100

    extract ClientWrapper private _answer* methods

--- src/infrastructure/client_wrapper.py
+++ src/infrastructure/client_wrapper.py
@@ -56,7 +56,7 @@ class ClientWrapper:
         debug: bool = False,
     ) -> QueryResult:
         """
-        Retrieves a simple response from the Mistral AI client.
+        Retrieves a simple response from the LLM client.
         """
         if prev_messages:
             complete_messages = list(prev_messages)
@@ -69,68 +69,64 @@ class ClientWrapper:
         messages: list[ChatMessage] = [
             complete_msg.chat_msg for complete_msg in complete_messages
         ]
-        content: str
-        role: str
+        assert model.platform
         if model.platform == Platform.OpenAI:
-            assert (
-                self._openai_client
-            ), "OpenAI client not defined. Did you forget to provide an api key for OpenAI API?"
-            openai_messages: Any = [
-                {
-                    "role": msg.role,
-                    "content": msg.content,
-                }
-                for msg in messages
-            ]
-            openai_chat_completion = self._openai_client.chat.completions.create(
-                messages=openai_messages,
-                model="gpt-3.5-turbo",
-            )
-            openai_chat_msg = openai_chat_completion.choices[0].message
-            assert isinstance(openai_chat_msg.content, str)
-            content = openai_chat_msg.content
-            role = openai_chat_msg.role
+            chat_msg = self._answer_using_openai(model, messages)
         else:
-            mistral_messages = [
-                MistralChatMessage(role=msg.role, content=msg.content)
-                for msg in messages
-            ]
-
-            assert model.platform == Platform.Mistral
-            model_name = model.model_name
-
-            try:
-                mistral_chat_msg = self.get_mistral_platform_chat_response(
-                    model_name, mistral_messages
-                )
-            except MistralConnectionException:
-                raise RuntimeError(
-                    "Error de conexión con la API de Mistral. Por favor, revise su conexión a internet."
-                ) from None
-
-            del mistral_messages
-            assert isinstance(mistral_chat_msg.content, str)
-            content = mistral_chat_msg.content
-            role = mistral_chat_msg.role
-        chat_msg = ChatMessage(role, content)
+            chat_msg = self._answer_using_mistral(model, messages)
         if debug:
             print(chat_msg)
             breakpoint()
         complete_messages.append(CompleteMessage(chat_msg, model))
-        return QueryResult(content, complete_messages)
+        return QueryResult(chat_msg.content, complete_messages)
 
-    def get_mistral_platform_chat_response(
-        self, model_name: ModelName, messages: list[MistralChatMessage]
-    ) -> MistralChatMessage:
+    def _answer_using_openai(
+        self, model: Model, messages: Sequence[ChatMessage]
+    ) -> ChatMessage:
+        assert (
+            self._openai_client
+        ), "OpenAI client not defined. Did you forget to provide an api key for OpenAI API?"
+        openai_messages: Any = [
+            {
+                "role": msg.role,
+                "content": msg.content,
+            }
+            for msg in messages
+        ]
+        openai_chat_completion = self._openai_client.chat.completions.create(
+            messages=openai_messages,
+            model=model.model_name,
+        )
+        openai_chat_msg = openai_chat_completion.choices[0].message
+        assert isinstance(openai_chat_msg.content, str)
+        content = openai_chat_msg.content
+        role = openai_chat_msg.role
+        return ChatMessage(role, content)
+
+    def _answer_using_mistral(
+        self, model: Model, messages: Sequence[ChatMessage]
+    ) -> ChatMessage:
+        assert model.platform == Platform.Mistral
+        mistral_messages = [
+            MistralChatMessage(role=msg.role, content=msg.content) for msg in messages
+        ]
         assert (
             self._mistralai_client
         ), "Mistral AI client not defined. Did you forget to provide an api key for Mistral API?"
-        chat_response = self._mistralai_client.chat(
-            model=model_name,
-            messages=messages,
-        )
+
+        try:
+            chat_response = self._mistralai_client.chat(
+                model=model.model_name,
+                messages=mistral_messages,
+            )
+        except MistralConnectionException:
+            raise RuntimeError(
+                "Error de conexión con la API de Mistral. Por favor, revise su conexión a internet."
+            ) from None
         choices = chat_response.choices
-        return choices[0].message
+        mistral_chat_msg = choices[0].message
+        assert isinstance(mistral_chat_msg.content, str)
+        return ChatMessage(mistral_chat_msg.role, mistral_chat_msg.content)
 
     def define_system_prompt(
         self,
