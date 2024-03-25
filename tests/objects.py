e8375b583fc2ec9920bbae2f265f07e5b6f6a170
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-25 11:34:07 +0100

    create convert_text_to_conversation_object()

--- tests/objects.py
+++ tests/objects.py
@@ -32,3 +32,24 @@ COMPLETE_MESSAGES_1 = [
         Model(None, ModelName("model_2")),
     ),
 ]
+
+TEXT_2 = """\
+[META id=0002]
+
+[META schema_version=0.2]
+[META number_of_messages=2]
+[META current_time=2023-05-20 13:00:02]
+
+[ROLE USER]
+¿What is 2 plus 2?
+
+[ROLE ASSISTANT model=model_1]
+2 plus 2 is 4"""
+
+COMPLETE_MESSAGES_2 = [
+    CompleteMessage(ChatMessage(role="user", content="¿What is 2 plus 2?"), None),
+    CompleteMessage(
+        ChatMessage(role="assistant", content="2 plus 2 is 4"),
+        Model(None, ModelName("model_1")),
+    ),
+]
