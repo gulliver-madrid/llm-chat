49c7bdc8be8371726393f3c6a4d4ca05af126428
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-28 18:23:53 +0100

    improve models_data, using a mapping

--- src/models_data.py
+++ src/models_data.py
@@ -1,15 +1,21 @@
-from typing import Final, Sequence
+from typing import Final, Mapping, Sequence
 
-from src.models.model_choice import MISTRAL_MODEL_PREFIX
 from src.models.shared import Model, ModelName, Platform
 
-
-mistral_models: Final[Sequence[str]] = ["tiny", "small", "medium", "large-2402"]
-openai_models: Final[Sequence[str]] = ["gpt-3.5-turbo", "gpt-4-1106-preview"]
+models_data: Final[Mapping[Platform, Sequence[str]]] = {
+    Platform.Mistral: (
+        "mistral-tiny",
+        "mistral-small",
+        "mistral-medium",
+        "mistral-large-2402",
+    ),
+    Platform.OpenAI: ("gpt-3.5-turbo", "gpt-4-1106-preview"),
+}
 
 
 def get_models() -> Sequence[Model]:
-    return [
-        Model(Platform.Mistral, ModelName(MISTRAL_MODEL_PREFIX + "-" + model))
-        for model in mistral_models
-    ] + [Model(Platform.OpenAI, ModelName(model)) for model in openai_models]
+    models: list[Model] = []
+    for platform, model_name_strings in models_data.items():
+        for model_name_str in model_name_strings:
+            models.append(Model(platform, ModelName(model_name_str)))
+    return models
