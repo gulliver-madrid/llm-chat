9d4dd1f3e17f4be02592fa1e20e0212704181bfc
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-03-30 18:13:32 +0100

    feat: allow defining a system prompt

--- src/infrastructure/client_wrapper.py
+++ src/infrastructure/client_wrapper.py
@@ -131,3 +131,9 @@ class ClientWrapper:
         )
         choices = chat_response.choices
         return choices[0].message
+
+    def define_system_prompt(
+        self,
+        prompt: str,
+    ) -> list[CompleteMessage]:
+        return [CompleteMessage(chat_msg=ChatMessage("system", prompt))]
