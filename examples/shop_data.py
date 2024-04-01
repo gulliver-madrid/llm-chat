7f7a9a3d71ea76aaacbb7becbf85326beb845398
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-01 12:55:12 +0200

    refactor shop_data.py

--- examples/shop_data.py
+++ examples/shop_data.py
@@ -1,12 +1,18 @@
-from typing import NewType, Sequence, TypedDict
+from collections.abc import Sequence
 import csv
 from pathlib import Path
+from typing import Any, NewType, TypedDict
 
 ProductRef = NewType("ProductRef", str)
 
 
+class Name(TypedDict):
+    english: str
+    spanish: str
+
+
 class Product(TypedDict):
-    name: dict[str, str]
+    name: Name
     price: float
     ref: ProductRef
 
@@ -18,16 +24,15 @@ def get_products_data() -> ProductsData:
     path = Path(__file__).parent / "items.csv"
     with open(path, "r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
-        items: list[Product] = []
-        for row in reader:
-            # Convertir cada fila del CSV en un diccionario
-            item: Product = {
-                "ref": ProductRef(row["ref"]),
-                "name": {
-                    "english": row["english_name"],
-                    "spanish": row["spanish_name"],
-                },
-                "price": float(row["price"]),
-            }
-            items.append(item)
-    return items
+        return [parse_product(row) for row in reader]
+
+
+def parse_product(row: dict[str, Any]) -> Product:
+    return {
+        "ref": ProductRef(row["ref"]),
+        "name": {
+            "english": row["english_name"],
+            "spanish": row["spanish_name"],
+        },
+        "price": float(row["price"]),
+    }
