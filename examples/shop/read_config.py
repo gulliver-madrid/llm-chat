bc345e353dc78cad53f1b16213a31db5fc26f898
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-03 13:51:50 +0200

    use ConfigReader

--- examples/shop/read_config.py
+++ examples/shop/read_config.py
@@ -5,38 +5,41 @@ from pathlib import Path
 default_config_file = Path(__file__).parent / "config.toml"
 
 
-def read_use_system_config(config_file: Path | str = default_config_file) -> bool:
-    default_config = {"use_system": True}
-    try:
-        if Path(config_file).exists():
-            with open(config_file, "r", encoding="utf-8") as file:
-                config: Any = tomlkit.load(file)
-            value = cast(bool, config.get("use_system", default_config["use_system"]))
+class ConfigReader:
+    def __init__(self, config_file: str | Path | None = None):
+        self._config_file: str | Path = config_file or default_config_file
+
+    def _get_config_doc(self) -> Any | None:
+        if not Path(self._config_file).exists():
+            print(f"Error al leer el archivo de configuración")
+            return None
+        with open(self._config_file, "r", encoding="utf-8") as file:
+            config: Any = tomlkit.load(file)
+        return config
+
+    def read_use_system_config(self) -> bool:
+        default_config = {"use_system": True}
+        if doc := self._get_config_doc():
+            value = cast(
+                bool,
+                doc.get("use_system", default_config["use_system"]),
+            )
             return value
         else:
             return default_config["use_system"]
-    except Exception as e:
-        print(f"Error al leer el archivo de configuración: {e}")
-        return default_config["use_system"]
 
-
-def read_model_config(config_file: Path | str = default_config_file) -> str | None:
-    try:
-        if Path(config_file).exists():
-            with open(config_file, "r", encoding="utf-8") as file:
-                config: Any = tomlkit.load(file)
-            value = config.get("model")
+    def read_model_config(self) -> str | None:
+        if doc := self._get_config_doc():
+            value = doc.get("model")
             assert isinstance(value, str)
             return value
         else:
             return None
-    except Exception as e:
-        print(f"Error al leer el archivo de configuración: {e}")
-        return None
 
 
 if __name__ == "__main__":
-    use_system = read_use_system_config()
+    config_reader = ConfigReader()
+    use_system = config_reader.read_use_system_config()
     print(f"Use system: {use_system}")
-    model = read_model_config()
+    model = config_reader.read_model_config()
     print(f"Model: {model}")
