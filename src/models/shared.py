be5b9e8ed275a7207f1c33ff225304f67568edec
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-03 15:49:26 +0200

    move define_system_prompt, WrongFunctionName

--- src/models/shared.py
+++ src/models/shared.py
@@ -36,3 +36,13 @@ def extract_chat_messages(
     complete_messages: Sequence[CompleteMessage],
 ) -> list[ChatMessage]:
     return [complete_chat.chat_msg for complete_chat in complete_messages]
+
+
+def define_system_prompt(
+    prompt: str, *, use_system: bool = True
+) -> list[CompleteMessage]:
+    return [
+        CompleteMessage(
+            chat_msg=ChatMessage("system" if use_system else "user", prompt)
+        )
+    ]
