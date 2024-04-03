fe6b4a387c21ab1e1dde6221ee47f6ff6b8bb595
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-03 23:57:19 +0200

    refactor ConfigReader

--- examples/shop/read_config.py
+++ examples/shop/read_config.py
@@ -1,6 +1,8 @@
+from types import NoneType
 from typing import Any, cast
-import tomlkit
 from pathlib import Path
+
+import tomlkit
 
 default_config_file = Path(__file__).parent / "config.toml"
 
@@ -8,33 +10,33 @@ default_config_file = Path(__file__).parent / "config.toml"
 class ConfigReader:
     def __init__(self, config_file: str | Path | None = None):
         self._config_file: str | Path = config_file or default_config_file
+        self._config = self._get_config_doc()
+
+    def read_use_system_config(self) -> bool:
+        default = True
+        if doc := self._config:
+            value = cast(
+                bool,
+                doc.get("use_system", default),
+            )
+            return value
+        return default
+
+    def read_model_config(self) -> str | None:
+        if doc := self._config:
+            value = doc.get("model")
+            assert isinstance(value, (NoneType, str))
+            return value
+        return None
 
     def _get_config_doc(self) -> Any | None:
         if not Path(self._config_file).exists():
-            print(f"Error al leer el archivo de configuración")
+            if self._config_file != default_config_file:
+                print(f"No se encontró el archivo de configuración")
             return None
         with open(self._config_file, "r", encoding="utf-8") as file:
             config: Any = tomlkit.load(file)
         return config
-
-    def read_use_system_config(self) -> bool:
-        default_config = {"use_system": True}
-        if doc := self._get_config_doc():
-            value = cast(
-                bool,
-                doc.get("use_system", default_config["use_system"]),
-            )
-            return value
-        else:
-            return default_config["use_system"]
-
-    def read_model_config(self) -> str | None:
-        if doc := self._get_config_doc():
-            value = doc.get("model")
-            assert isinstance(value, str)
-            return value
-        else:
-            return None
 
 
 if __name__ == "__main__":
