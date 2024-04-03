973842933c0a93eccee3323a8dc4d71715c58e0e
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-03 13:41:20 +0200

    shop: read model from read_config

--- examples/shop/read_config.py
+++ examples/shop/read_config.py
@@ -20,6 +20,23 @@ def read_use_system_config(config_file: Path | str = default_config_file) -> boo
         return default_config["use_system"]
 
 
+def read_model_config(config_file: Path | str = default_config_file) -> str | None:
+    try:
+        if Path(config_file).exists():
+            with open(config_file, "r", encoding="utf-8") as file:
+                config: Any = tomlkit.load(file)
+            value = config.get("model")
+            assert isinstance(value, str)
+            return value
+        else:
+            return None
+    except Exception as e:
+        print(f"Error al leer el archivo de configuración: {e}")
+        return None
+
+
 if __name__ == "__main__":
     use_system = read_use_system_config()
     print(f"Use system: {use_system}")
+    model = read_model_config()
+    print(f"Model: {model}")
