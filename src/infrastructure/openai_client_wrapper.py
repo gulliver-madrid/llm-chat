37080e12428f70b5d4e7cdfa0e75dc27857d1a06
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-04 13:51:09 +0200

    extract convert_to_openai_msg()

--- src/infrastructure/openai_client_wrapper.py
+++ src/infrastructure/openai_client_wrapper.py
@@ -1,5 +1,6 @@
+from collections.abc import Mapping
 from types import NoneType
-from typing import Any, Sequence, cast
+from typing import Any, Iterable, Sequence, cast
 
 from openai import OpenAI
 
@@ -23,24 +24,15 @@ class OpenAIClientWrapper:
         *,
         tools: list[dict[str, Any]] | None = None,
     ) -> ChatMessage:
-        openai_messages: Any = []
-        for msg in messages:
-            openai_msg: Any = {
-                "role": msg.role,
-                "content": msg.content,
-            }
-            if msg.tool_calls:
-                openai_msg["tool_calls"] = msg.tool_calls
-            if msg.tool_call_id:
-                openai_msg["tool_call_id"] = msg.tool_call_id
-            openai_messages.append(openai_msg)
-
         logger.info(f"{model=}")
-        logger.info(format_var("openai_messages", openai_messages))
         logger.info(f"{tools=}")
 
+        openai_messages = [convert_to_openai_msg(msg) for msg in messages]
+
+        logger.info(format_var("openai_messages", openai_messages))
+
         openai_chat_completion = self._openai_client.chat.completions.create(
-            messages=openai_messages,
+            messages=cast_openai_messages(openai_messages),
             model=model.model_name,
             tools=cast(Any, tools),
         )
@@ -52,3 +44,21 @@ class OpenAIClientWrapper:
         content = openai_chat_msg.content
         role = openai_chat_msg.role
         return ChatMessage(role, content or "", tool_calls=openai_chat_msg.tool_calls)
+
+
+def convert_to_openai_msg(msg: ChatMessage) -> Mapping[str, object]:
+    openai_msg: dict[str, object] = {
+        "role": msg.role,
+        "content": msg.content,
+    }
+    if tool_calls := msg.tool_calls:
+        assert isinstance(tool_calls, list)
+        openai_msg["tool_calls"] = tool_calls
+    if id_ := msg.tool_call_id:
+        assert isinstance(id_, str)
+        openai_msg["tool_call_id"] = id_
+    return openai_msg
+
+
+def cast_openai_messages(openai_messages: list[Mapping[str, object]]) -> Iterable[Any]:
+    return cast(Iterable[Any], openai_messages)
