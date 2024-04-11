8b62270c46a0d16bfdb27f5ebb9b769329fd518b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-11 12:10:18 +0200

    rename tests

--- tests/test_load_conversation.py
+++ tests/test_load_conversation.py
@@ -29,7 +29,7 @@ CASES = [
 ]
 
 
-def test_load_conversation_from_text() -> None:
+def test_parse_conversation_from_text() -> None:
     for text, expected_conversation in CASES:
         conversation = convert_text_to_conversation_object(
             text, preserve_model=True, check_model_exists=False
@@ -37,7 +37,7 @@ def test_load_conversation_from_text() -> None:
         assert conversation == expected_conversation
 
 
-def test_load_messages_from_text() -> None:
+def test_parse_messages_from_text() -> None:
     for text, messages in [
         (TEXT_1, COMPLETE_MESSAGES_1),
         (TEXT_2, COMPLETE_MESSAGES_2),
