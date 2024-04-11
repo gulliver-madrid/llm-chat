0e9a39bc6c16e56d9e16be16a528cf360a18692f
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-11 12:29:44 +0200

    extract test_tags

--- tests/test_parse_conversation.py
+++ tests/test_parse_conversation.py
@@ -1,4 +1,3 @@
-from src.models.parsed_line import ParsedLine, TagType
 from src.models.serialization import (
     Conversation,
     convert_conversation_into_messages,
@@ -46,42 +45,3 @@ def test_parse_messages_from_text() -> None:
             text, preserve_model=True, check_model_exists=False
         )
         assert result == messages
-
-
-NO_TAGS = [
-    "",
-    # something before
-    " [META id=0001]",
-    # something after
-    "[META id=0001] ",
-    # missing space
-    "[METAid]",
-]
-TAGS_WITH_TYPES = [
-    ("[META id=0001]", TagType.META),
-    ("[ROLE ASSISTANT model=model_1]", TagType.ROLE),
-]
-
-
-def test_is_tag() -> None:
-    for no_tag in NO_TAGS:
-        assert_is_tag(False, no_tag)
-
-    for tag, _ in TAGS_WITH_TYPES:
-        assert_is_tag(True, tag)
-
-
-def test_get_tag_type() -> None:
-    for no_tag in NO_TAGS:
-        assert_tag_type(None, no_tag)
-
-    for tag, expected_type in TAGS_WITH_TYPES:
-        assert_tag_type(expected_type, tag)
-
-
-def assert_is_tag(expected: bool, string: str) -> None:
-    assert expected == ParsedLine(string).is_tag()
-
-
-def assert_tag_type(expected: TagType | None, string: str) -> None:
-    assert expected == ParsedLine(string).get_tag_type()
