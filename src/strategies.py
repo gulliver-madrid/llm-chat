58e93aee3ac68fa2feed3503d535b64398082de3
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-12 14:23:01 +0200

    use ShowModelAction

--- src/strategies.py
+++ src/strategies.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from src.generic_view import Raw
+from src.models.model_wrapper import ModelWrapper
 from src.models.shared import CompleteMessage, define_system_prompt
 from src.view import View
 
@@ -19,3 +21,20 @@ class EstablishSystemPromptAction(ActionStrategy):
         # TODO: allow multiline
         self._prev_messages[:] = [define_system_prompt(remaining_input)]
         self._view.write_object("System prompt established")
+
+
+class ShowModelAction(ActionStrategy):
+    def __init__(self, view: View, model_wrapper: ModelWrapper):
+        self._view = view
+        self._model_wrapper = model_wrapper
+
+    def execute(self, remaining_input: str) -> None:
+        if remaining_input.strip():
+            raise ValueError(remaining_input)
+        self._show_model()
+
+    def _show_model(self) -> None:
+        assert self._model_wrapper.model
+        self._view.display_neutral_msg(
+            Raw(f"El modelo actual es {self._model_wrapper.model.model_name}")
+        )
