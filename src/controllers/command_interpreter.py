b0ecd54b44d333f319c0f0a880d222c507bf3e0a
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-13 15:49:10 +0200

    use command mapping

--- src/controllers/command_interpreter.py
+++ src/controllers/command_interpreter.py
@@ -1,5 +1,7 @@
+from collections.abc import Mapping
 from dataclasses import dataclass
 from enum import Enum
+from typing import Final
 
 
 class ActionType(Enum):
@@ -25,7 +27,22 @@ class CommandNoValid(Exception):
         super().__init__(f"Comando no válido: {wrong_command}")
 
 
+command_map = {
+    ActionType.EXIT: ("q", "quit", "exit"),
+    ActionType.HELP: ("h", "help"),
+    ActionType.DEBUG: ("d", "debug"),
+    ActionType.LOAD_CONVERSATION: ("load",),
+    ActionType.LOAD_MESSAGES: ("load_msgs",),
+    ActionType.NEW_CONVERSATION: ("new",),
+    ActionType.CHANGE_MODEL: ("change",),
+    ActionType.SHOW_MODEL: ("show",),
+    ActionType.SYSTEM_PROMPT: ("sys", "system"),
+}
+
+
 class CommandInterpreter:
+    def __init__(self) -> None:
+        self._commands_to_actions: Final = self._build_commands_to_actions()
 
     def parse_user_input(self, raw_query: str) -> tuple[Action, str]:
         splitted = raw_query.strip().split()
@@ -36,26 +53,17 @@ class CommandInterpreter:
         if not first.startswith("/"):
             return (Action(ActionType.CONTINUE_CONVERSATION), raw_query)
 
-        match command := first[1:]:
-            case "q" | "quit" | "exit":
-                action = Action(ActionType.EXIT)
-            case "h" | "help":
-                action = Action(ActionType.HELP)
-            case "d" | "debug":
-                action = Action(ActionType.DEBUG)
-            case "load":
-                action = Action(ActionType.LOAD_CONVERSATION)
-            case "load_msgs":
-                action = Action(ActionType.LOAD_MESSAGES)
-            case "new":
-                action = Action(ActionType.NEW_CONVERSATION)
-            case "change":
-                action = Action(ActionType.CHANGE_MODEL)
-            case "show":
-                action = Action(ActionType.SHOW_MODEL)
-            case "sys" | "system":
-                action = Action(ActionType.SYSTEM_PROMPT)
-            case _:
-                raise CommandNoValid(command)
-        assert action
+        command = first[1:]
+        action_type = self._commands_to_actions.get(command)
+        if not action_type:
+            raise CommandNoValid(command)
+        action = Action(action_type)
         return (action, " ".join(splitted[1:]))
+
+    def _build_commands_to_actions(self) -> Mapping[str, ActionType]:
+        commands_to_actions: dict[str, ActionType] = {}
+        for action_type, strings in command_map.items():
+            for s in strings:
+                assert s not in commands_to_actions
+                commands_to_actions[s] = action_type
+        return commands_to_actions
