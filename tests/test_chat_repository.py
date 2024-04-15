1fb25bfc9939d0548726058feed1323c385d2d7c
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-15 14:05:12 +0200

    extract ChatRepositoryImplementer

--- tests/test_chat_repository.py
+++ tests/test_chat_repository.py
@@ -2,14 +2,18 @@ from unittest.mock import Mock
 
 from src.python_modules.FileSystemWrapper.file_manager import FileManager
 
-from src.infrastructure.repository import ChatRepository
+from src.infrastructure.repository import ChatRepository, ChatRepositoryImplementer
 
 
 def test_create_chat_repository() -> None:
     file_manager_mock = Mock(spec=FileManager)
     file_manager_mock.get_children.return_value = []
+    chat_repository_implementer_mock = Mock(spec=ChatRepositoryImplementer)
 
-    ChatRepository(file_manager_mock)
+    ChatRepository(
+        file_manager=file_manager_mock,
+        chat_repository_implementer=chat_repository_implementer_mock,
+    )
 
     file_manager_mock.mkdir_if_not_exists.assert_called()
 
@@ -17,3 +21,5 @@ def test_create_chat_repository() -> None:
     assert any(call.args[0].name == "data" for call in calls)
     assert any(call.args[0].name == "chats" for call in calls)
     assert not any(call.args[0].name == "another_name" for call in calls)
+
+    chat_repository_implementer_mock.move_chat_files_from_data_dir_to_chat_dir.assert_called_once()
