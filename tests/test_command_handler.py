30b1f133965040e8ee85367b623394a740987e58
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-17 10:55:16 +0200

    add docstrings to test_command_handler.py

--- tests/test_command_handler.py
+++ tests/test_command_handler.py
@@ -18,9 +18,15 @@ from tests.objects import TEXT_1
 
 
 class TestCommandHandlerBase:
-    """This class should not be directly instantiated"""
+    """
+    Base class for testing command handlers. This class should not be directly instantiated.
+    Therefore it should not include tests.
+    """
 
     def setup_method(self) -> None:
+        """
+        Sets up necessary mock objects and initial state for each test method.
+        """
         self.mock_view = Mock(spec=View)
         self.mock_select_model_controler = Mock(spec=SelectModelController)
         self.mock_repository = Mock(spec=ChatRepository)
@@ -40,10 +46,16 @@ class TestCommandHandlerBase:
 
 class TestCommandHandlerMultipleActions(TestCommandHandlerBase):
     def test_process_system(self) -> None:
+        """
+        Tests that system prompts can be processed and the result is added to previous
+        messages correctly.
+        """
         system_prompt = "System prompt string"
+
         self.command_handler.process_action(
             Action(ActionType.SYSTEM_PROMPT), system_prompt
         )
+
         assert len(self.prev_messages_stub) == 1
         first_chat_msg = self.prev_messages_stub[0].chat_msg
         assert first_chat_msg.role == "system"
@@ -53,6 +65,10 @@ class TestCommandHandlerMultipleActions(TestCommandHandlerBase):
 
 class TestCommandHandlerShowModel(TestCommandHandlerBase):
     def setup_method(self) -> None:
+        """
+        Sets up additional variables and inherits the base setup, define a multiline
+        user prompt to ensure that tests avoid infinite loops in mutation testing.
+        """
         super().setup_method()
         self.model_name = ModelName("Model name test")
         # if a line is not sent before the `end` command, there is a risk
@@ -60,12 +76,18 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
         self.user_prompt_lines = ["something more", "end"]
 
     def _select_model(self) -> None:
+        """
+        Private helper method for selecting a model using the SelectModelController mock.
+        """
         self.mock_select_model_controler.select_model.return_value = Model(
             None, self.model_name
         )
         self.command_handler.prompt_to_select_model()
 
-    def test_show_model_works_when_no_extra_prompt(self) -> None:
+    def test_show_model_works_when_no_extra_chat(self) -> None:
+        """
+        Tests that displaying the current model works correctly when no extra chat is present.
+        """
         remaining = ""
         self._select_model()
 
@@ -77,6 +99,10 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
         )
 
     def test_show_model_fails_when_there_is_extra_prompt(self) -> None:
+        """
+        Tests that an error is raised when extraneous text is present in the prompt after
+        the command to show the model.
+        """
         remaining = "some text"
         self._select_model()
 
@@ -86,6 +112,9 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
             )
 
     def test_chat_with_model(self) -> None:
+        """
+        Simulates a conversation with the model, checking the correct continuation of interaction.
+        """
         self.mock_view.input_extra_line.side_effect = self.user_prompt_lines
         self.mock_client_wrapper.get_simple_response.side_effect = (
             get_simple_response_stub
@@ -98,14 +127,14 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
             Action(ActionType.CONTINUE_CONVERSATION), remaining
         )
         self.mock_view.print_interaction.assert_called()
-        calls = self.mock_view.print_interaction.mock_calls
-        assert len(calls) == 1
-
+        print_interaction_calls = self.mock_view.print_interaction.mock_calls
+        assert len(print_interaction_calls) == 1
         assert len(self.prev_messages_stub) == 2
 
     def test_chat_with_model_using_placeholder(self) -> None:
         """
-        Checks that substitutions are made in the user's message, and a model response is requested.
+        Checks that substitutions are made in the user's message, and a model response
+        is requested.
         """
         user_substitutions = {"$0something": "anything"}
         self.mock_view.input_extra_line.side_effect = self.user_prompt_lines
@@ -133,24 +162,29 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
         assert user_message_replaced.chat_msg.content == expected_user_content_replaced
 
     def test_chat_with_model_continue(self) -> None:
-        """Simula una conversacion que continua (aunque el modelo repite lo mismo por simplificar)"""
-
+        """
+        Simulates a conversation with the model, checking the correct continuation of
+        interaction (although the model repeats the same thing for simplicity).
+        """
+        # arrange
         self.mock_view.input_extra_line.side_effect = (
             self.user_prompt_lines + self.user_prompt_lines
         )
         self.mock_client_wrapper.get_simple_response.side_effect = (
             get_simple_response_stub
         )
-
         user_prompt = "hello, how are you?"
         self._select_model()
 
+        # act
         self.command_handler.process_action(
             Action(ActionType.CONTINUE_CONVERSATION), user_prompt
         )
         self.command_handler.process_action(
             Action(ActionType.CONTINUE_CONVERSATION), "then, again, how are you?"
         )
+
+        # assert
         self.mock_client_wrapper.get_simple_response.assert_called()
         self.mock_view.print_interaction.assert_called()
         calls = self.mock_view.print_interaction.mock_calls
@@ -158,6 +192,9 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
         assert len(self.prev_messages_stub) == 4
 
     def test_show_help_wait_user_press_enter(self) -> None:
+        """
+        Tests the help display functionality, requiring the user to press enter to proceed.
+        """
         remaining = ""
         self._select_model()
 
@@ -172,6 +209,10 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
         assert "enter" in prompt_for_user.value.lower()
 
     def test_debug_command_works(self) -> None:
+        """
+        Tests the debug functionality within the model interaction, ensuring the debug flag
+        is correctly sent to ClientWrapper instance.
+        """
         remaining = "some text"
         self.mock_view.input_extra_line.side_effect = self.user_prompt_lines
         self.mock_client_wrapper.get_simple_response.return_value = QueryResult("", [])
@@ -185,6 +226,10 @@ class TestCommandHandlerShowModel(TestCommandHandlerBase):
         assert calls[0].kwargs["debug"] == True
 
     def test_load_conversation(self) -> None:
+        """
+        Tests the loading of a specific conversation by ID, checking calls to retrieval
+        and display of conversation data.
+        """
         remaining = "42"
         self.mock_repository.load_conversation_as_text.return_value = TEXT_1
 
