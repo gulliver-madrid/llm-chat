46a0aaf90507939238b7f17f268af749bba24cb6
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-17 20:03:49 +0200

    update mutmut_config.py: add is_enum_or_similar()

--- mutmut_config.py
+++ mutmut_config.py
@@ -4,15 +4,29 @@ from typing import Any
 STYLE_TAGS_REGEX = r"""^[A-Z_]+ = StyleTag\("\[[a-z_]+\]"\)$"""
 
 
-def pre_mutation(context: Any):
+def is_enum_or_similar(line: str) -> bool:
+    # Regex that checks if the line starts with 4 spaces and the constant name is identical
+    # to the assigned value
+    regex = r'^ {4}([A-Z_]+) = "\1"$'
+    # Check if there is a match
+    return bool(re.match(regex, line))
 
-    print(context.filename)
+
+def pre_mutation(context: Any) -> None:
+    lines: list[str] = []
+    lines.append("\n" * 2 + context.filename)
+    lines.append(context.current_source_line)
+    lines.append(f"mutation_id: {context.mutation_id}")
+    print("\n".join(lines))
     if "python_modules" in context.filename:
         print("skipped:", context.filename)
         context.skip = True
         return
     if re.match(STYLE_TAGS_REGEX, context.current_source_line.strip()):
-        print(f"skipped line: {context.line}")
+        context.skip = True
+    elif is_enum_or_similar(context.current_source_line):
         context.skip = True
     else:
         print("not skipped")
+    if context.skip:
+        print("skipped line")
