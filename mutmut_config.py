b3661d90419231a158f0ca259ef8ecaae7374b03
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-17 19:53:24 +0200

    build: clean mutmut_config.py

--- mutmut_config.py
+++ mutmut_config.py
@@ -7,10 +7,6 @@ STYLE_TAGS_REGEX = r"""^[A-Z_]+ = StyleTag\("\[[a-z_]+\]"\)$"""
 def pre_mutation(context: Any):
 
     print(context.filename)
-    # print(f"{context.should_mutate(node)=}")
-    # print(dir(context))
-    print(dir(context.config))
-    # print((context.config.paths_to_mutate))
     if "python_modules" in context.filename:
         print("skipped:", context.filename)
         context.skip = True
