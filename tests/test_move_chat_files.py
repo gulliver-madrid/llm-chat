2428e77efb3f2cda108140d5a7f10dde9b2a311a
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-18 19:39:04 +0200

    test: add test_move_chat_files

---
+++ tests/test_move_chat_files.py
@@ -0,0 +1,50 @@
+from typing import Sequence
+from unittest.mock import MagicMock, Mock
+
+from src.python_modules.FileSystemWrapper.file_manager import FileManager
+from src.python_modules.FileSystemWrapper.path_wrapper import PathWrapper
+
+from src.infrastructure.chat_repository.implementer import (
+    ChatRepositoryImplementer,
+    SafeFileContentMover,
+)
+
+
+def test_move_chat_files_from_data_dir_to_chat_dir() -> None:
+    mock_file_manager = Mock(spec=FileManager)
+    mock_data_dir = Mock(spec=PathWrapper)
+    mock_chat_dir = MagicMock(spec=PathWrapper)
+    repository_implementer = ChatRepositoryImplementer()
+    repository_implementer.init(mock_data_dir, mock_chat_dir, mock_file_manager)
+    content_mover = Mock(spec=SafeFileContentMover)
+    repository_implementer._content_mover = (  # pyright: ignore [reportPrivateUsage]
+        content_mover
+    )
+
+    files = [Mock(spec=PathWrapper), Mock(spec=PathWrapper)]
+    files[0].name = "no_es_un_chat.txt"
+    files[1].name = "0001.chat"
+
+    def mock_get_children(data_dir: PathWrapper) -> Sequence[PathWrapper]:
+        if data_dir is mock_data_dir:
+            return files
+        elif data_dir is mock_chat_dir:
+            return []
+        else:
+            raise ValueError()
+
+    def mock_is_dir(p: PathWrapper) -> bool:
+        if p in files:
+            return False
+        return True
+
+    mock_file_manager.get_children.side_effect = mock_get_children
+    mock_file_manager.path_exists.return_value = True
+    mock_file_manager.path_is_dir.side_effect = mock_is_dir
+
+    mock_dest_path = Mock(spec=PathWrapper)
+    mock_chat_dir.__truediv__.return_value = mock_dest_path
+
+    repository_implementer.move_chat_files_from_data_dir_to_chat_dir()
+
+    content_mover.move_content.assert_called_once()
