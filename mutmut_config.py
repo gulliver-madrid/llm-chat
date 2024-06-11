5493cb37ee9df2ad30b2c8875d8e5594dceea8e8
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-06-11 12:37:13 +0200

    build: mutmut_config.py: make debug optional

--- mutmut_config.py
+++ mutmut_config.py
@@ -3,6 +3,8 @@ from typing import Any
 
 STYLE_TAGS_REGEX = r"""^[A-Z_]+ = StyleTag\("\[[a-z_]+\]"\)$"""
 
+extra_debug = False
+
 
 def is_enum_or_similar(line: str) -> bool:
     # Regex that checks if the line starts with 4 spaces and the constant name is identical
@@ -16,22 +18,25 @@ def pre_mutation(context: Any) -> None:
     lines: list[str] = []
     lines.append("\n" * 2 + context.filename)
     lines.append(context.current_source_line)
-    lines.append(f"index: {context.index}")
-    # lines.append(f"mutation_id: {context.mutation_id}")
-    print("\n".join(lines))
+
+    if extra_debug:
+        print("\n".join(lines))
     if "python_modules" in context.filename:
-        print("skipped:", context.filename)
+        if extra_debug:
+            print("file skipped")
         context.skip = True
         return
     if re.search(r"\blogger\b", context.current_source_line):
         context.skip = True
-    if re.match(STYLE_TAGS_REGEX, context.current_source_line.strip()):
+    elif re.match(STYLE_TAGS_REGEX, context.current_source_line.strip()):
         context.skip = True
     elif is_enum_or_similar(context.current_source_line):
         context.skip = True
     elif context.current_source_line.strip().startswith("assert "):
         context.skip = True
-    else:
-        print("not skipped")
-    if context.skip:
-        print("skipped line")
+
+    if extra_debug:
+        if context.skip:
+            print("skipped line")
+        else:
+            print("not skipped")
