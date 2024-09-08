8de10371ef8bb54fd6527727db333de2e17650e1
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-09-08 13:47:40 +0200

    refactor: get_interaction_styled_view() to make it more readable

--- src/view/views.py
+++ src/view/views.py
@@ -9,10 +9,17 @@ def get_interaction_styled_view(
     time_manager: TimeManagerProtocol, model: ModelName, query: Raw, content: Raw
 ) -> str:
     """Returns the styled representation of an interaction between user and model"""
-    lines: list[str] = []
-    lines.append("\n" + time_manager.get_current_time())
-    lines.append("\n" + highlight_role(Raw("USER: ")) + escape_for_rich(query))
-    lines.append(
-        "\n" + highlight_role(Raw(model.upper() + ": ")) + escape_for_rich(content)
-    )
-    return "\n".join(lines)
+    time_repr = time_manager.get_current_time()
+    user_repr = _get_user_repr(query)
+    model_repr = _get_model_repr(model, content)
+    return "\n".join("\n" + text for text in [time_repr, user_repr, model_repr])
+
+
+def _get_model_repr(model: ModelName, content: Raw) -> str:
+    role_model_repr = highlight_role(Raw(model.upper() + ": "))
+    return role_model_repr + escape_for_rich(content)
+
+
+def _get_user_repr(query: Raw) -> str:
+    role_user_repr = highlight_role(Raw("USER: "))
+    return role_user_repr + escape_for_rich(query)
